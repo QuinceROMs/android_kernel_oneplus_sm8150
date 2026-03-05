bcfa879b77a066f50da388272dce917014914367
Quince <quinceroms@gmail.com>
2026-03-05 15:16:38 +0100

    thermal: Create named symlinks for thermal zones
    Create sysfs symlinks in /sys/class/thermal/ using the thermal zone
    type name, providing a human-readable alias for the numbered
    thermal_zoneN directories.
    
    For example, a thermal zone with type "skin-therm" registered as
    thermal_zone18 will get a symlink:
      /sys/class/thermal/skin-therm -> ../../devices/virtual/thermal/thermal_zone18
    
    This is needed because OnePlus Camera reads skin temperature from the
    hardcoded path /sys/class/thermal/skin-therm/temp for thermal
    protection (flash auto-stop, recording thermal limit). Without this
    symlink, the camera's DeviceStateManagerImpl throws:
      E OnePlus Camera: DeviceStateManagerImpl checkSkinThermalTemperature
        java.io.FileNotFoundException: /sys/class/thermal/skin-therm/temp
    
    The symlinks are created during thermal_zone_device_register() and
    removed during thermal_zone_device_unregister(). Non-fatal: if a
    duplicate type name causes sysfs_create_link() to fail, the numbered
    path still works normally.
    
    Change-Id: Ib59ce61d0b3544b09503430d12291f6e
    Signed-off-by: Quince <quinceroms@gmail.com>
    (cherry picked from commit 7f8fcc1ab05d066ccabc9598136353597b3bde11)
    (cherry picked from commit c465ec7c9f3837800f59199d2b479b7225a04f9b)
    (cherry picked from commit 52545e1fa357439c8809d000051248158fdbc44b)

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1371,6 +1371,11 @@ thermal_zone_device_register(const char *type, int trips, int mask,
 	if (result)
 		goto remove_device_groups;
 
+	/* Create named symlink: /sys/class/thermal/<type> -> thermal_zoneN */
+	if (tz->type[0])
+		sysfs_create_link(&thermal_class.p->subsys.kobj,
+				  &tz->device.kobj, tz->type);
+
 	for (count = 0; count < trips; count++) {
 		if (tz->ops->get_trip_type(tz, count, &trip_type))
 			set_bit(count, &tz->trips_disabled);
@@ -1505,6 +1510,8 @@ void thermal_zone_device_unregister(struct thermal_zone_device *tz)
 	thermal_set_governor(tz, NULL);
 
 	thermal_remove_hwmon_sysfs(tz);
+	if (tz->type[0])
+		sysfs_remove_link(&thermal_class.p->subsys.kobj, tz->type);
 	ida_simple_remove(&thermal_tz_ida, tz->id);
 	ida_destroy(&tz->ida);
 	mutex_destroy(&tz->lock);
