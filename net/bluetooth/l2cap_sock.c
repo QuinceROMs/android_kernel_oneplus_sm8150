e9cee636662a7b22d449bf3cb62b443fd75d83a5
Siwei Zhang <oss@fourdim.xyz>
2026-06-04 09:45:38 -0400

    Bluetooth: L2CAP: use chan timer to close channels in cleanup_listen()
    [ Upstream commit 8c8e620467a7b51562dbcefbd1f09f288d7d710d ]
    
    l2cap_chan_close() removes the channel from conn->chan_l, which
    must be done under conn->lock.  cleanup_listen() runs under the
    parent sk_lock, so acquiring conn->lock would invert the
    established conn->lock -> chan->lock -> sk_lock order.
    
    Instead of calling l2cap_chan_close() directly, schedule
    l2cap_chan_timeout with delay 0 to close the channel
    asynchronously.  The timeout handler already acquires conn->lock
    and chan->lock in the correct order.
    
    The timer is only armed when chan->conn is still set: if it is
    already NULL, l2cap_conn_del() has already processed this channel
    (l2cap_chan_del + l2cap_sock_teardown_cb + l2cap_sock_close_cb),
    so there is nothing left to do.  If l2cap_conn_del() races in
    after the timer is armed, __clear_chan_timer() inside
    l2cap_chan_del() cancels it; if the timer has already fired, the
    handler returns harmlessly because chan->conn was cleared.
    
    Fixes: 3df91ea20e74 ("Bluetooth: Revert to mutexes from RCU list")
    Cc: <stable@vger.kernel.org> # 0b58004: Bluetooth: fix UAF in l2cap_sock_cleanup_listen() vs l2cap_conn_del()
    Signed-off-by: Siwei Zhang <oss@fourdim.xyz>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 3634cbdc2eb414b69ffa752ddbe5e0458518e321)
    Signed-off-by: Quince <quinceroms@gmail.com>

--- net/bluetooth/l2cap_sock.c
+++ net/bluetooth/l2cap_sock.c
@@ -1233,7 +1233,12 @@ static void l2cap_sock_cleanup_listen(struct sock *parent)
 	BT_DBG("parent %p state %s", parent,
 	       state_to_string(parent->sk_state));
 
-	/* Close not yet accepted channels */
+	/* Close not yet accepted channels.
+	 *
+	 * Since we cannot call l2cap_chan_close() without conn->lock,
+	 * schedule l2cap_chan_timeout to close the channel; it already
+	 * acquires conn->lock -> chan->lock in the correct order.
+	 */
 	while ((sk = bt_accept_dequeue(parent, NULL))) {
 		struct l2cap_chan *chan = l2cap_pi(sk)->chan;
 
@@ -1242,12 +1247,14 @@ static void l2cap_sock_cleanup_listen(struct sock *parent)
 
 		l2cap_chan_hold(chan);
 		l2cap_chan_lock(chan);
-
-		__clear_chan_timer(chan);
-		l2cap_chan_close(chan, ECONNRESET);
-		l2cap_sock_kill(sk);
-
+		/* Since we cannot call l2cap_chan_close() without
+		 * conn->lock, schedule its timer to trigger the close
+		 * and cleanup of this channel.
+		 */
+		if (chan->conn)
+			__set_chan_timer(chan, 0);
 		l2cap_chan_unlock(chan);
+
 		l2cap_chan_put(chan);
 	}
 }
